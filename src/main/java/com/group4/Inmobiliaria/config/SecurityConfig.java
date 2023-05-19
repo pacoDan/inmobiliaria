620e2bd65f70102ee598d4a26b3fd072b414af80
Lucas Uhrich <lucasuhrich318@gmail.com>
2023-05-18 21:05:03 -0300

    SecurityConfig solucion de errores

--- src/main/java/com/group4/Inmobiliaria/config/SecurityConfig.java
+++ src/main/java/com/group4/Inmobiliaria/config/SecurityConfig.java
@@ -26,21 +26,23 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
     protected void configure(HttpSecurity http) throws Exception {
         http
                 .authorizeRequests()
-                .antMatchers("").hasRole("ADMIN")
-                .antMatchers("").permitAll()
-                .and().formLogin()
-                .loginPage("/login")
-                .loginProcessingUrl("/logincheck")
-                .usernameParameter("email")
-                .passwordParameter("password")
-                .defaultSuccessUrl("/")
-                .permitAll()
-                .and().logout()
-                .logoutUrl("/logout")
-                .logoutSuccessUrl("/")
-                .permitAll()
-                .and().csrf()
-                .disable();
+                    //.antMatchers("").hasRole("ADMIN")
+                    .antMatchers("\"/css/*\", \"/js/*\", \"/img/*\", \"/**\"").permitAll()
+                .and()
+                    .formLogin()
+                    .loginPage("/login")
+                    .loginProcessingUrl("/logincheck")
+                    .usernameParameter("email")
+                    .passwordParameter("password")
+                    .defaultSuccessUrl("/")
+                    .permitAll()
+                .and()
+                    .logout()                    
+                    .logoutSuccessUrl("/")
+                    .permitAll()
+                .and()
+                    .csrf()
+                    .disable();
 
     }
 
