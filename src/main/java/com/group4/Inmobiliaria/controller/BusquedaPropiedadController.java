da19bd9f8e20638c7aaadd25358aa50f8686a32b
pacoDan <jhonpaco@frba.utn.edu.ar>
2023-05-20 15:52:49 -0300

    :fire: controller solo puesto para probar en main, no para GET y POST, probando

--- src/main/java/com/group4/Inmobiliaria/controller/BusquedaPropiedadController.java
+++ src/main/java/com/group4/Inmobiliaria/controller/BusquedaPropiedadController.java
@@ -4,26 +4,34 @@ import com.group4.Inmobiliaria.entidades.Propiedad;
 import com.group4.Inmobiliaria.service.BusquedaPropiedadService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
 import java.util.ArrayList;
+import java.util.List;
 
 @Controller
 public class BusquedaPropiedadController {
     @Autowired
     BusquedaPropiedadService propiedadService;
 
-    @GetMapping("/probando")
-    public void probando() {
-        ArrayList<Propiedad> listaPropiedades = (ArrayList<Propiedad>) propiedadService.obtenerPropiedades();
+//    @GetMapping("/")
+//    public String probando(Model model) {
+//        System.out.println("probando--------------------");
+//        this.mostrarPropiedades();
+////        model.addAttribute("propiedades", propiedades);
+//        return "index";
+//    }
 
+    private void mostrarPropiedades() {
+        ArrayList<Propiedad> listaPropiedades = (ArrayList<Propiedad>) propiedadService.obtenerPropiedades();
         for (Propiedad propiedad : listaPropiedades) {
             System.out.println(propiedad.toString());
         }
     }
 
     public void someMethod() {
-        System.out.println("CONTROLLER----------------------------------------------------------------------------------");
+        System.out.println("CONTROLLER inicio ----------------------------------------------------------------------------------");
         ArrayList<Propiedad> listaPropiedades = (ArrayList<Propiedad>) propiedadService.obtenerPropiedades();
 
         for (Propiedad propiedad : listaPropiedades) {
