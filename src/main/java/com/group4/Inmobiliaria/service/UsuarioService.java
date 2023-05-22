23420efda54d3e6dd44743e02b634732c6b7e2d0
Lucas Uhrich <lucasuhrich318@gmail.com>
2023-05-22 18:05:41 -0300

    UsuarioService - getAll

--- src/main/java/com/group4/Inmobiliaria/service/UsuarioService.java
+++ src/main/java/com/group4/Inmobiliaria/service/UsuarioService.java
@@ -1,5 +1,17 @@
 package com.group4.Inmobiliaria.service;
 
+import com.group4.Inmobiliaria.entidades.Usuario;
+import com.group4.Inmobiliaria.repository.UsuarioRepository;
+import java.util.List;
+import org.springframework.beans.factory.annotation.Autowired;
+
 public class UsuarioService {
+
+    @Autowired
+    UsuarioRepository usuarioRepository;
+
+    public List<Usuario> getAllUsers() {
+        return usuarioRepository.findAll();
+    }
 
 }
