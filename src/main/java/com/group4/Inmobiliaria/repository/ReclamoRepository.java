a3f440a6992ffb337dab94f50553fa9940e894dd
Lucas Uhrich <lucasuhrich318@gmail.com>
2023-06-05 17:41:06 -0300

    Backend - Reclamo - metodos y querys personalizados para Repository y Service

--- src/main/java/com/group4/Inmobiliaria/repository/ReclamoRepository.java
+++ src/main/java/com/group4/Inmobiliaria/repository/ReclamoRepository.java
@@ -1,7 +1,13 @@
 package com.group4.Inmobiliaria.repository;
 
 import com.group4.Inmobiliaria.entidades.Reclamo;
+import java.util.List;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface ReclamoRepository extends JpaRepository<Reclamo, String> {
+    
+    List<Reclamo>findByCliente_id(String id);
+    
+    List<Reclamo>findByEnte_id(String id);
+    
 }
