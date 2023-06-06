63dce06a99278af222bb3d7e5441a7fca45d30a9
Ludmila Muñoz <ludmi.m.1109@gmail.com>
2023-06-05 22:07:33 -0300

    Services y Repository con query personalizada de entidades

--- src/main/java/com/group4/Inmobiliaria/repository/CitaRepository.java
+++ src/main/java/com/group4/Inmobiliaria/repository/CitaRepository.java
@@ -1,7 +1,13 @@
 package com.group4.Inmobiliaria.repository;
 
 import com.group4.Inmobiliaria.entidades.Cita;
+import java.util.List;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface CitaRepository extends JpaRepository<Cita, String> {
+
+    public List<Cita> findByEmisor_id(String id);
+
+    public List<Cita> findByReceptor_id(String id);
+
 }
