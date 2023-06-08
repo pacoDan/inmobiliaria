7fad27efcd786f80bccbc7460ed0b1edccfce1cc
Lucas Uhrich <lucasuhrich318@gmail.com>
2023-06-08 16:14:21 -0300

    Frontend - Botones solicitar compra/alquiler

--- src/main/java/com/group4/Inmobiliaria/controller/SolicitudController.java
+++ src/main/java/com/group4/Inmobiliaria/controller/SolicitudController.java
@@ -1,10 +1,12 @@
 package com.group4.Inmobiliaria.controller;
 
 import com.group4.Inmobiliaria.entidades.Cliente;
+import com.group4.Inmobiliaria.entidades.Propiedad;
 import com.group4.Inmobiliaria.entidades.Solicitud;
 import com.group4.Inmobiliaria.enums.EstadoSolicitud;
 import com.group4.Inmobiliaria.enums.TipoProcedimiento;
 import com.group4.Inmobiliaria.enums.TipoSolicitud;
+import com.group4.Inmobiliaria.service.PropiedadService;
 import com.group4.Inmobiliaria.service.SolicitudService;
 import com.group4.Inmobiliaria.utils.Session;
 import java.util.Date;
@@ -12,6 +14,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 @RequestMapping("/solicitud")
@@ -19,6 +22,9 @@ public class SolicitudController {
 
     @Autowired
     SolicitudService solicitudService;
+    
+    @Autowired    
+    PropiedadService propiedadService;
 
     @PostMapping("/guardar/compra")
     public String guardarSolicitudCompra(Solicitud solicitud) {
@@ -39,9 +45,11 @@ public class SolicitudController {
     }
 
     @PostMapping("/guardar/alquiler")
-    public String guardarSolicitudAlquiler(Solicitud solicitud) {
+    public String guardarSolicitudAlquiler(Solicitud solicitud, @RequestParam(name = "idPropiedad") String idPropiedad) {
         Cliente cliente = (Cliente) Session.getUserSession();
-
+        
+        Propiedad propiedad = propiedadService.encontrarById(idPropiedad);                
+        
         solicitud.setTipoProcedimiento(TipoProcedimiento.SOLICITUD);
         solicitud.setTipoSolicitud(TipoSolicitud.ALQUILER);
         solicitud.setEstadoSolicitud(EstadoSolicitud.PENDIENTE);
@@ -50,6 +58,10 @@ public class SolicitudController {
         solicitud.setHoraEmision(new Date());
         
         solicitud.setEmisor(cliente);
+        
+        solicitud.setPropiedad(propiedad);
+        
+        solicitud.setReceptor(propiedad.getPropietario());
         
         solicitudService.guardarSolicitud(solicitud);
 
